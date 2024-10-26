e5e54b0077799fb51f4e435ecc41b93833d3354c
Kiran Avinash <kiranavinash.sode@gmail.com>
2024-10-26 22:57:28 +0530

    Fixed issue in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { LanguageProvider } from './context/languageContext.jsx'; 
+import { LanguageProvider } from './context/LanguageContext'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
