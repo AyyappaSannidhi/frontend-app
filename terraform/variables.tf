23988eac41f1921ee9565fad0d6f808f737a6a3b
Kiran Avinash <kiranavinash.sode@gmail.com>
2024-10-28 17:56:58 +0530

    removed domain and hosted zone as we are using cloudfare as CDN.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,5 @@
-variable "domain_name" {
-  description = "The domain name for the website"
-  type        = string
-  default     = "ayyappasannidhi.org"
-}
-
 variable "s3_bucket_name" {
   description = "S3 bucket name"
   type        = string
-  default     = "ayyappasannidhi.org"
-}
-
-variable "hosted_zone_id" {
-  description = "Route 53 hosted zone ID"
-  type        = string
-  default     = "Z10120513M1Z2K7FI1YMG"
+  default     = "www.ayyappasannidhi.org"
 }
