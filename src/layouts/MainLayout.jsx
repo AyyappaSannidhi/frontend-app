4780976e6aef9158bf3bb272d852ffdf0c70f2d6
Kiran Avinash <kiranavinash.sode@gmail.com>
2024-11-08 22:19:59 +0530

    Added functionality to scroll up to page top when switching pages using react router.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,11 +6,13 @@ import Footer from "../components/Footer";
 import ScrollingBanner from "../components/ScrollingBanner"
 import NavBar from "../components/NavBar";
 import BackToTopButton from "../components/BackTopButton";
+import ScrollToTop from "../components/ScrollToTop";
 
 
 const MainLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBar/>
       <ScrollingBanner/>
       <ToastContainer />
